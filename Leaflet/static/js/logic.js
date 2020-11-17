840caf955bb1bc4079a98364c23348785f1362bd
Victoria Cruz <victoriacruz32@gmail.com>
2020-11-16 21:12:42 -0600

    Updating File Structure

--- Leaflet/static/js/logic.js
+++ Leaflet/static/js/logic.js
@@ -1,7 +1,7 @@
 // Creating our initial map object
 // We set the longitude, latitude, and the starting zoom level for sf
 // This gets inserted into the div with an id of 'map' in index.html
-var myMap = L.map("mapid", {
+var myMap = L.map("map", {
     center: [37.7749, -122.4194],
     zoom: 5
   });
