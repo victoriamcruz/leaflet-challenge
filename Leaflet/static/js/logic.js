790d440f3e9e57f90eb55f63525716778b5edd0f
Victoria Cruz <victoriacruz32@gmail.com>
2020-11-17 21:58:27 -0600

    Final Push

--- Leaflet/static/js/logic.js
+++ Leaflet/static/js/logic.js
@@ -3,20 +3,21 @@
 // This gets inserted into the div with an id of 'map' in index.html
 var myMap = L.map("map", {
     center: [37.7749, -122.4194],
-    zoom: 5
+    zoom: 2
   });
   
   // Adding a tile layer (the background map image) to our map
   // We use the addTo method to add objects to our map
-  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
     maxZoom: 18,
-    id: "mapbox.streets",
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
     accessToken: API_KEY
   }).addTo(myMap);
 
   // Store our API endpoint
-// var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 let voldata  = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
 //  GET color radius call to the query URL
